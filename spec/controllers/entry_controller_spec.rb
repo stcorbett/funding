079d90dcb433aafe7f6ac4971a4021078e9b515a
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2009-11-10 14:09:55 -0600

    initial files

---
+++ spec/controllers/entry_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe EntryController do
+
+  #Delete this example and add some real ones
+  it "should use EntryController" do
+    controller.should be_an_instance_of(EntryController)
+  end
+
+end
