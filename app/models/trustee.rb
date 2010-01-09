4e5f1980d7318a9f8959259c1a4a29d41ebbb158
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2010-01-08 22:37:12 -0600

    method to update trustee list

--- app/models/trustee.rb
+++ app/models/trustee.rb
@@ -5,4 +5,15 @@ class Trustee < ActiveRecord::Base
     TrusteeMailer.deliver_trustee_app(self)
   end
 
+  def self.li_links
+    Trustee.all.collect{|t| t.li_link  }
+  end
+  
+  def li_link
+    %(%li
+  = link_to "#{self.name}#{" (#{self.organization})" unless self.organization.blank?}", "#{self.url}"#{ %(
+  = link_to "@#{self.twitter.gsub("@", "")}", "http://www.twitter.com/#{self.twitter}") unless self.twitter.blank?}
+  )
+  end
+
 end
