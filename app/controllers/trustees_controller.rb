258994e14f16583626159c8b56af59f1a3127bb5
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2010-02-13 21:13:19 -0600

    copy update

--- app/controllers/trustees_controller.rb
+++ app/controllers/trustees_controller.rb
@@ -6,7 +6,7 @@ class TrusteesController < ApplicationController
   
   def create
     @trustee = Trustee.create(params[:trustee])
-    flash[:notice] = "Thanks!  You should recieve an email with more information about the grant."
+    flash[:notice] = "Thanks!  We will send information about the upcomming grants in the next few weeks."
     redirect_to :action => :index
   end
   
