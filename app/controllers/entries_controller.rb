079d2f5c872e6f06d9c9fe04823b1dab7820f6a4
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2010-03-01 14:10:50 -0600

    open applications for next round of grants

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,8 +7,8 @@ class EntriesController < ApplicationController
   end
 
   def create
-    #@entry = Entry.create(params[:entry])
-    flash[:notice] = "Entries are currently closed."
+    @entry = Entry.create(params[:entry])
+    flash[:notice] = "Your application has been received.  Thanks!  We will update you on selection process within a month."
     redirect_to :action => :index
   end
 
