fb172acbcd977dc63aedbe0833f066f33c160d5f
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2010-03-01 14:01:16 -0600

    print all trustee and applicant email addresses

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -5,5 +5,12 @@ class Subscriber < ActiveRecord::Base
     SubscriberMailer.deliver_subscribed(self)
     SubscriberMailer.deliver_new_subscriber(self)
   end
-
+  
+  def self.all_email_addresses
+    str = ""
+    Trustee.all.each{|t| str << "#{t.email}, " unless t.email.blank?}
+    Entry.all.each{|e| str << "#{e.email}, " unless e.email.blank?}
+    str.chomp(", ")
+  end
+  
 end
