106772139a06cb05f3c5613748db0273d4cde34f
Sean Corbett <sean@sean-corbetts-macbook-pro-2.local>
2010-04-02 00:36:22 -0500

    add a page to download the trustee and applicant email lists

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -5,5 +5,13 @@ class SubscribersController < ApplicationController
     flash[:notice] = "Thanks!  You will receive an email confirming your subscription."
     redirect_to :controller => :entries, :action => :index
   end
+  
+  def admin_list
+    redirect_to home_path unless params[:whodat] == "izadminhere4299"
+    @list = ''
+    Trustee.all.each{|t| @list << %("#{t.name}" #{t.email}, ) unless t.email.blank?}
+    Entry.all.each{|e| @list << %("#{e.name}" #{e.email},) unless e.email.blank?}
+    @list.chomp(", ")
+  end
 
 end
