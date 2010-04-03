e35aaa7b2bad4ec0a1e35da0b2d435f86ed43eb6
Mischa <f.mischa@gmail.com>
2010-04-03 17:24:31 -0500

    some user stuff

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100210171945) do
+ActiveRecord::Schema.define(:version => 20100403221531) do
 
   create_table "entries", :force => true do |t|
     t.string   "company_name"
@@ -39,6 +39,24 @@ ActiveRecord::Schema.define(:version => 20100210171945) do
     t.string   "organization"
     t.string   "twitter"
     t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
